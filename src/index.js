0e1b06555fe04dde0926f658867aaf23c97b1fd6
Marcin Głowacki <marcinglowacki01@gmail.com>
2019-02-19 20:48:08 +0100

    Commiting for code review in case of problems with template title scaling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
 import _ from 'lodash';
-import {
-  changeSlide,
-  currentSlide,
-  handleTouchEnd,
-  handleTouchMove,
-  handleTouchStart
-} from './slider';
-import { ECONNABORTED } from 'constants';
+import {changeSlide, currentSlide} from './slider';
 function component() {
   let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -16,27 +9,12 @@ function component() {
 document.body.appendChild(component());
 
 // slider functionality
-
 document.querySelector('.prev').addEventListener('click', function(){changeSlide(-1)});
 document.querySelector('.next').addEventListener('click', function(){changeSlide(1)});
 document.querySelector('#dot1').addEventListener('click', function(){currentSlide(1)});
 document.querySelector('#dot2').addEventListener('click', function(){currentSlide(2)});
 document.querySelector('#dot3').addEventListener('click', function(){currentSlide(3)});
 // slider functionality
-
-document.querySelector('.HoverOver').style.color = "red";
-
-document.querySelector('#sliderImg1').addEventListener('touchstart', handleTouchStart)
-document.querySelector('#sliderImg1').addEventListener('touchmove', handleTouchMove)
-document.querySelector('#sliderImg1').addEventListener('touchend', handleTouchEnd)
-
-document.querySelector('#sliderImg2').addEventListener('touchstart', handleTouchStart)
-document.querySelector('#sliderImg2').addEventListener('touchmove', handleTouchMove)
-document.querySelector('#sliderImg2').addEventListener('touchend', handleTouchEnd)
-
-document.querySelector('#sliderImg3').addEventListener('touchstart', handleTouchStart)
-document.querySelector('#sliderImg3').addEventListener('touchmove', handleTouchMove)
-document.querySelector('#sliderImg3').addEventListener('touchend', handleTouchEnd)
 
 
 
