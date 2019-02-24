ab2787453c0e42250e80287c9af797287ee0676a
Marcin Głowacki <marcinglowacki01@gmail.com>
2019-02-24 11:24:37 +0100

    Brought back slider functionalities

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import _ from 'lodash';
+import {
+  changeSlide,
+  currentSlide,
+  handleTouchEnd,
+  handleTouchMove,
+  handleTouchStart
+} from './slider';
 import {changeSlide, currentSlide} from './slider';
 function component() {
   let element = document.createElement('div');
@@ -15,6 +22,19 @@ document.querySelector('#dot1').addEventListener('click', function(){currentSlid
 document.querySelector('#dot2').addEventListener('click', function(){currentSlide(2)});
 document.querySelector('#dot3').addEventListener('click', function(){currentSlide(3)});
 // slider functionality
+document.querySelector('.HoverOver').style.color = "red";
+
+document.querySelector('#sliderImg1').addEventListener('touchstart', handleTouchStart)
+document.querySelector('#sliderImg1').addEventListener('touchmove', handleTouchMove)
+document.querySelector('#sliderImg1').addEventListener('touchend', handleTouchEnd)
+
+document.querySelector('#sliderImg2').addEventListener('touchstart', handleTouchStart)
+document.querySelector('#sliderImg2').addEventListener('touchmove', handleTouchMove)
+document.querySelector('#sliderImg2').addEventListener('touchend', handleTouchEnd)
+
+document.querySelector('#sliderImg3').addEventListener('touchstart', handleTouchStart)
+document.querySelector('#sliderImg3').addEventListener('touchmove', handleTouchMove)
+document.querySelector('#sliderImg3').addEventListener('touchend', handleTouchEnd)
 
 
 
