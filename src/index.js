5bf6701937799f63773ca08951d1fb3704775df3
unknown <maciekhnat@gmail.com>
2019-03-07 21:07:39 +0100

    changed mobile design to show more and less

--- src/index.js
+++ src/index.js
@@ -23,6 +23,55 @@ document.querySelector('#dot2').addEventListener('click', function(){currentSlid
 document.querySelector('#dot3').addEventListener('click', function(){currentSlide(3)});
 // slider functionality
 // document.querySelector('.HoverOver').style.color = "red";
+var x = window.matchMedia("(max-width: 480px)")
+isMobile(x) // Call listener function at run time
+x.addListener(isMobile) // Attach listener function on state changes
+var z = window.matchMedia("(min-width: 481px)")
+isNotMobile(z) // Call listener function at run time
+z.addListener(isNotMobile) // Attach listener function on state changes
+
+function isNotMobile(z) {
+  if (z.matches) { // If media query matches
+    document.getElementById("meet_us_grid").style.display = "grid";
+    document.getElementById("meetus_expand").remove();
+    document.getElementById("meetus_expand_button").remove();
+
+  }
+}
+
+function more(){
+
+  if (document.getElementById("more").innerHTML === "więcej +") {
+    document.getElementById("meet_us_grid").style.display = "grid";
+    document.getElementById("more").innerHTML = "mniej -";
+
+  } else {
+    if (document.getElementById("more").innerHTML === "mniej -") {
+        document.getElementById("meet_us_grid").style.display = "none";
+        document.getElementById("more").innerHTML = "więcej +";
+    }
+  }
+  
+}
+
+function isMobile(x) {
+  if (x.matches) { // If media query matches
+    document.getElementById("meet_us_grid").style.display = "none";
+    
+    document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand")
+    document.getElementById("meetus_expand").innerHTML = "<p>Nasz zespół</p><p>Poznaj nasz zespół</p>"
+    document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand_button")
+    document.getElementById("meetus_expand_button").innerHTML = "<p id='more'>więcej +</p>"
+    
+    document.getElementById("more").addEventListener("click", more);
+    
+  }
+}
+// if (document.getElementById("more").innerHTML === "mniej -") {
+//   document.getElementById("meet_us_grid").style.display = "none";
+//   document.getElementById("more").innerHTML = "więcej +";
+// } 
+
 
 document.querySelector('#sliderImg1').addEventListener('touchstart', handleTouchStart)
 document.querySelector('#sliderImg1').addEventListener('touchmove', handleTouchMove)
