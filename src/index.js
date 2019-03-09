9101f92bd38d42b96b0548aa378711a7243c9b7c
unknown <maciekhnat@gmail.com>
2019-03-09 14:01:02 +0100

    put meetus functionality into separate module

--- src/index.js
+++ src/index.js
@@ -6,22 +6,20 @@ import {
   handleTouchMove,
   handleTouchStart
 } from './slider';
+import {MeetUsMobileBehavior, MeetUsDesktopBehavior} from './meetus';
 import VanillaTilt from 'vanilla-tilt';
 
-function component() {
-  let element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  return element;
-}
+// media queries variables//
+var x = window.matchMedia("(max-width: 480px)")
+var z = window.matchMedia("(min-width: 481px)")
+// media queries variables//
 
-document.body.appendChild(component());
 
-// slider functionality
-
+// slider functionality//
 document.querySelector('#dot1').addEventListener('click', function(){currentSlide(1)});
 document.querySelector('#dot2').addEventListener('click', function(){currentSlide(2)});
 document.querySelector('#dot3').addEventListener('click', function(){currentSlide(3)});
-var x = window.matchMedia("(max-width: 480px)")
+
 isMobile(x) // Call listener function at run time
 x.addListener(isMobile) // Attach listener function on state changes
 let element = document.querySelector("#sliderImg1")
@@ -35,66 +33,22 @@ function isMobile(x) {
     VanillaTilt.init(element);
   }
 }
+// slider functionality//
+
+
+//meetus functionality//
+MeetUsMobileBehavior(x) // Call listener function at run time
+x.addListener(MeetUsMobileBehavior) // Attach listener function on state changes
+
+MeetUsDesktopBehavior(z) // Call listener function at run time
+z.addListener(MeetUsDesktopBehavior) // Attach listener function on state changes
+//
 
 
 
-// slider functionality
-// document.querySelector('.HoverOver').style.color = "red";
-var x = window.matchMedia("(max-width: 480px)")
-isMobile(x) // Call listener function at run time
-x.addListener(isMobile) // Attach listener function on state changes
-var z = window.matchMedia("(min-width: 481px)")
-isNotMobile(z) // Call listener function at run time
-z.addListener(isNotMobile) // Attach listener function on state changes
 
-function isNotMobile(z) {
-  if (z.matches) { // If media query matches
-    try {
-      document.getElementById("meet_us_grid").style.display = "grid";
-      document.getElementById("meetus_expand").remove();
-      document.getElementById("meetus_expand_button").remove();
-    } catch (error) {
-      console.error(error);
-      // expected output: ReferenceError: nonExistentFunction is not defined
-      // Note - error messages will vary depending on browser
-    }
-    
 
-  }
-}
 
-function more(){
-
-  if (document.getElementById("more").innerHTML === "więcej +") {
-    document.getElementById("meet_us_grid").style.display = "grid";
-    document.getElementById("more").innerHTML = "mniej -";
-
-  } else {
-    if (document.getElementById("more").innerHTML === "mniej -") {
-        document.getElementById("meet_us_grid").style.display = "none";
-        document.getElementById("more").innerHTML = "więcej +";
-    }
-  }
-  
-}
-
-function isMobile(x) {
-  if (x.matches) { // If media query matches
-    document.getElementById("meet_us_grid").style.display = "none";
-    
-    document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand")
-    document.getElementById("meetus_expand").innerHTML = "<p>Nasz zespół</p><p>Poznaj nasz zespół</p>"
-    document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand_button")
-    document.getElementById("meetus_expand_button").innerHTML = "<p id='more'>więcej +</p>"
-    
-    document.getElementById("more").addEventListener("click", more);
-    
-  }
-}
-// if (document.getElementById("more").innerHTML === "mniej -") {
-//   document.getElementById("meet_us_grid").style.display = "none";
-//   document.getElementById("more").innerHTML = "więcej +";
-// } 
 
 
 // document.querySelector('#sliderImg1').addEventListener('touchstart', handleTouchStart)
---
+++ src/meetus.js
@@ -0,0 +1,51 @@
+function more() {
+
+    if (document.getElementById("more").innerHTML === "więcej +") {
+        document.getElementById("meet_us_grid").style.display = "grid";
+        document.getElementById("more").innerHTML = "mniej -";
+
+    } else {
+        if (document.getElementById("more").innerHTML === "mniej -") {
+            document.getElementById("meet_us_grid").style.display = "none";
+            document.getElementById("more").innerHTML = "więcej +";
+        }
+    }
+
+}
+
+
+function MeetUsDesktopBehavior(z) {
+    if (z.matches) { // If media query matches
+        try {
+            if (document.getElementById("meet_us_grid")){
+                document.getElementById("meet_us_grid").style.display = "grid";
+                document.getElementById("meetus_expand").remove();
+                document.getElementById("meetus_expand_button").remove();
+            }
+        } catch (error) {
+            console.error(error);
+            // expected output: ReferenceError: nonExistentFunction is not defined
+            // Note - error messages will vary depending on browser
+        }
+    }
+}
+
+function MeetUsMobileBehavior(x) {
+    if (x.matches) { // If media query matches
+        document.getElementById("meet_us_grid").style.display = "none";
+
+        document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand")
+        document.getElementById("meetus_expand").innerHTML = "<p>Nasz zespół</p><p>Poznaj nasz zespół</p>"
+        document.getElementById("meet_us").appendChild(document.createElement("div")).setAttribute("id", "meetus_expand_button")
+        document.getElementById("meetus_expand_button").innerHTML = "<p id='more'>więcej +</p>"
+
+        document.getElementById("more").addEventListener("click", more);
+
+    }
+}
+
+module.exports = {
+    more,
+    MeetUsMobileBehavior,
+    MeetUsDesktopBehavior
+};
