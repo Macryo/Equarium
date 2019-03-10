fe622c844aeaab6c78e2ea450fb54da48c159a35
unknown <maciekhnat@gmail.com>
2019-03-10 15:08:24 +0100

    Squashed commit of the following:
    commit 2a5b2d382d04ae7c590c8f7813a212a9b7572562
    Author: unknown <maciekhnat@gmail.com>
    Date:   Sun Mar 10 15:02:16 2019 +0100
    
        code refactoring contact form
    
    commit 089113145c79c4345ac52fd96076d29608e8157b
    Author: unknown <maciekhnat@gmail.com>
    Date:   Sun Mar 10 14:47:38 2019 +0100
    
        contact form code refactoring

--- src/form.js
+++ src/form.js
@@ -15,50 +15,40 @@ export default function validate(e) {
 
     let FieldArray = [name, email, phone, msg];
 
+    let errorMsg = document.getElementById("errorMsg");
+    document.getElementById("errorMsg").style.display = 'flex';
+    e.preventDefault()
+
     for (let i = 0; i < FieldArray.length; i++) {
         if (FieldArray[i].value == "") {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Pole "${FieldArray[i].name}" nie może być puste`;
-            e.preventDefault();
-            return
-        }
-
-        if (!emailReg.test(email.value)) {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Nieprawidłowy adres email`;
-            e.preventDefault();
-            return
-        }
-        if (!phoneReg.test(phone.value)) {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Nieprawidłowy numer telefonu, przykladowy numer to "111222333"`;
-            e.preventDefault();
-            return
-        }
-        if (name.value.length < 5 || name.value.length > 50) {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Nieprawidłowe imię i nazwisko`;
-            e.preventDefault();
-            return
-        }
-        if (msg.value.length < 5) {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Wiadomość jest za krótka`;
-            e.preventDefault();
-            return
-        }
-        if (msg.value.length > 150) {
-            document.getElementById("errorMsg").style.display = 'flex';
-            document.getElementById("errorMsg").innerHTML = `Wiadomość jest za krótka`;
+            errorMsg.innerHTML = `Pole "${FieldArray[i].name}" nie może być puste`;
             e.preventDefault();
             return
         }
     }
-
-    e.preventDefault()
-    console.log('sieeema')
-    document.getElementById("errorMsg").style.display = 'none';
-    document.getElementById("successMsg").style.display = 'flex';
-    document.getElementById("successMsg").innerHTML = `Wiadomość wysłano pomyślnie`;
+    if (!emailReg.test(email.value)) {
+        errorMsg.innerHTML = `Nieprawidłowy adres email`;
+        return
+    }
+    else if (!phoneReg.test(phone.value)) {
+        errorMsg.innerHTML = `Nieprawidłowy numer telefonu, przykladowy numer to "111222333"`;  
+        return
+    }
+    else if (name.value.length < 5 || name.value.length > 50) {
+        errorMsg.innerHTML = `Nieprawidłowe imię i nazwisko`;
+        return
+    }
+    else if (msg.value.length < 5 || msg.value.length > 250) {
+        errorMsg.innerHTML = `Wiadomość powinna zawierać od 5 do 250 znaków`;
+        return
+    }
+    
+    else{
+        document.getElementById("errorMsg").style.display = 'none';
+        document.getElementById("successMsg").style.display = 'flex';
+        document.getElementById("successMsg").innerHTML = `Wiadomość wysłano pomyślnie`;
+    }
+    
+    
 }
 
