26c70fc6a5f5803de3632a607d7b6c43f914cb57
unknown <maciekhnat@gmail.com>
2019-04-18 15:34:48 +0200

    finished working header

--- src/header.js
+++ src/header.js
@@ -1,2 +1,33 @@
+export function expandNav() {
+    document.getElementById('nav').classList.remove("hidden");
+    document.getElementById('exit').classList.remove("hidden");
+    document.getElementById('hambi').classList.remove("hambi");
+    document.getElementById('logo').classList.remove("logo");
+    document.getElementById('face-left').classList.remove("hidden");
+    document.getElementById('face-right').classList.remove("hidden");
 
+    document.getElementById('exit').classList.add("exit");
+    document.getElementById('hambi').classList.add("hidden");
+    document.getElementById('logo').classList.add("hidden");
+    document.getElementById('nav').classList.add("expandNav");
+
+    document.getElementById('exit').addEventListener('click', exit)
+    for (let i = 0; i < 6; i++) {
+        document.getElementsByClassName('menu_child')[i].addEventListener('click', exit);
+    }
+}
+
+export function exit() {
+
+    document.getElementById('exit').classList.add("hidden");
+    document.getElementById('hambi').classList.add("hambi");
+    document.getElementById('logo').classList.add("logo");
+    document.getElementById('face-left').classList.add("hidden");
+    document.getElementById('face-right').classList.add("hidden");
+
+    document.getElementById('exit').classList.remove("exit");
+    document.getElementById('hambi').classList.remove("hidden");
+    document.getElementById('logo').classList.remove("hidden");
+    document.getElementById('nav').classList.remove("expandNav");
+}
 
--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {
 
 import { MeetUsMobileBehavior, MeetUsDesktopBehavior } from './meetus';
 import VanillaTilt from 'vanilla-tilt';
+import {expandNav, exit} from './header';
 //scroll animation
 import AOS from 'aos';
 AOS.init();
@@ -20,6 +21,9 @@ var x = window.matchMedia("(max-width: 480px)")
 var z = window.matchMedia("(min-width: 481px)")
 // media queries variables//
 
+//header
+document.getElementById('hambi').addEventListener('click', expandNav);
+//header
 
 // slider functionality//
 document.querySelector('#dot1').addEventListener('click', function(){currentSlide(1)});
@@ -90,35 +94,7 @@ z.addListener(MeetUsDesktopBehavior) // Attach listener function on state change
 
 
 
-document.getElementById('hambi').addEventListener('click', expandNav);
 
-function expandNav() {
-  document.getElementById('nav').classList.remove("hidden");
-  document.getElementById('exit').classList.remove("hidden");
-  document.getElementById('hambi').classList.remove("hambi");
-  document.getElementById('logo').classList.remove("logo");
-  document.getElementById('face-left').classList.remove("hidden");
-  document.getElementById('face-right').classList.remove("hidden");
 
-  document.getElementById('exit').classList.add("exit");
-  document.getElementById('hambi').classList.add("hidden");
-  document.getElementById('logo').classList.add("hidden");
-  document.getElementById('nav').classList.add("expandNav");
 
-  document.getElementById('exit').addEventListener('click', exit)
-}
-
-function exit() {
-  
-  document.getElementById('exit').classList.add("hidden");
-  document.getElementById('hambi').classList.add("hambi");
-  document.getElementById('logo').classList.add("logo");
-  document.getElementById('face-left').classList.add("hidden");
-  document.getElementById('face-right').classList.add("hidden");
-
-  document.getElementById('exit').classList.remove("exit");
-  document.getElementById('hambi').classList.remove("hidden");
-  document.getElementById('logo').classList.remove("hidden");
-  document.getElementById('nav').classList.remove("expandNav");
-}
 
